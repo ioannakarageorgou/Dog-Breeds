104b43ec7ea6fa7cc9af9557fa84061686942fa4
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-01-19 10:45:29 +0200

    Project setup

---
+++ Dog Breeds/Dog Breeds/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dog Breeds
+//
+//  Created by Ioanna Karageorgou on 19/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
