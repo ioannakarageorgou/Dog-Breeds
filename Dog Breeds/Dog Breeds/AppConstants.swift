f35453c936cbbf6d28377aefe249766bf154a26d
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-01-19 11:00:57 +0200

    Add network manager

---
+++ Dog Breeds/Dog Breeds/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  Dog Breeds
+//
+//  Created by Ioanna Karageorgou on 19/1/24.
+//
+
+import Foundation
+
+struct AppConstants {
+    // Network
+    static let baseUrl = "https://dog.ceo/api/breeds/list/all"
+}
