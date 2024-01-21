58dc018a386527f7b05b320ea35dccb8ee51a62a
Ioanna Karageorgou <ioannakarageorgou@gmail.com>
2024-01-21 11:17:23 +0200

    Add mock network service

---
+++ Dog Breeds/Dog BreedsTests/BreedsRepositoryTests.swift
@@ -0,0 +1,8 @@
+//
+//  BreedsRepositoryTests.swift
+//  Dog BreedsTests
+//
+//  Created by Ioanna Karageorgou on 21/1/24.
+//
+
+import Foundation
---
+++ Dog Breeds/Dog BreedsTests/MockBreedsRepository.swift
@@ -0,0 +1,8 @@
+//
+//  MockBreedsRepository.swift
+//  Dog BreedsTests
+//
+//  Created by Ioanna Karageorgou on 21/1/24.
+//
+
+import Foundation
